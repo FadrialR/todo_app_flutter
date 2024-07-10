386581b5738a8f41c6679e121f020a670a82ef42
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2024-07-10 13:30:59 +0700

    Add App Bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/components/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,7 +8,25 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ToDo App"),
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/noah.jpg'),
+                ))
+          ],
+        ),
       ),
       body: Container(
         child: Text('This is Home screen'),
